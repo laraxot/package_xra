04b6041efedf848ea2d63ab686d368874ae48a30
marco xot <marco.sottana@gmail.com>
2018-12-19 10:08:07 +0100

    .

--- src/Traits/FormRequestTrait.php
+++ src/Traits/FormRequestTrait.php
@@ -2,5 +2,16 @@
 namespace XRA\XRA\Traits;
 
 trait FormRequestTrait{
-	
+	/**
+	 * Get the error messages for the defined validation rules.
+	 *
+	 * @return array
+	 */
+	public function messages(){
+		$class=str_replace('XRA\\Food\\Requests\\','',get_class());
+		$class=str_replace('\\','.',$class);
+		$class=snake_case($class);
+		$trad=trans('food::'.$class);
+		return $trad;
+	}
 }
