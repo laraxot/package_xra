786e3cf1eb971f9da3e7fcfbe7dd05d0e199e4ea
Mark Walter <dev@geekpiu.com>
2018-12-19 11:42:39 +0100

    Modifica su Trait per i request.
    	1) Gestisce il caso in cui non sia presente la traduzione della request (così non si rompe)
    	2) Gestisce una traduzione generica degli errori multilingua per i controlli comuni
    
    	modified:   src/Traits/FormRequestTrait.php

--- src/Traits/FormRequestTrait.php
+++ src/Traits/FormRequestTrait.php
@@ -1,17 +1,25 @@
 <?php
+
 namespace XRA\XRA\Traits;
 
-trait FormRequestTrait{
-	/**
-	 * Get the error messages for the defined validation rules.
-	 *
-	 * @return array
-	 */
-	public function messages(){
-		$class=str_replace('XRA\\Food\\Requests\\','',get_class());
-		$class=str_replace('\\','.',$class);
-		$class=snake_case($class);
-		$trad=trans('food::'.$class);
-		return $trad;
-	}
+trait FormRequestTrait
+{
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        $class = str_replace('XRA\\Food\\Requests\\', '', get_class());
+        $class = str_replace('\\', '.', $class);
+        $class = snake_case($class);
+        $trad = trans('food::generic');
+        $tradSpecific = trans('food::' . $class);
+        if (is_array($tradSpecific)) {
+            $trad = array_merge($trad, $tradSpecific);
+        }
+
+        return $trad;
+    }
 }
