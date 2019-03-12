fc3476c2b96a0630f29bdbb9249a35707b3f23c3
marco xot <marco.sottana@gmail.com>
2019-03-12 21:33:21 +0100

    .

--- src/XRAServiceProvider.php
+++ src/XRAServiceProvider.php
@@ -78,6 +78,54 @@ class XRAServiceProvider extends ServiceProvider
         });
     }
 
+    /*
+    //https://meritocracy.is/blog/2017/09/11/3-laravel-blade-directives-will-save-time/
+    // Add @var for Variable Assignment
+Blade::directive('var', function($expression) {
+
+  // Strip Open and Close Parenthesis
+  $expression = substr(substr($expression, 0, -1), 1);
+
+  list($variable, $value) = explode('\',', $expression, 2);
+
+  // Ensure variable has no spaces or apostrophes
+  $variable = trim(str_replace('\'', '', $variable));
+
+  // Make sure that the variable starts with $
+  if (!starts_with($variable, '$')) {
+    $variable = '$' . $variable;
+  }
+
+  $value = trim($value);
+  return "<?php {$variable} = {$value}; ?>";
+});
+*/
+/*
+// Add @asset markup
+Blade::directive('asset', function($file) {
+
+    $file = str_replace(['(', ')', "'"], '', $file);
+    $filename = $file;
+
+    // Internal file
+    if (!starts_with($file, '//') && !starts_with($file, 'http')) {
+        $version = File::lastModified(public_path() . '/' . $file);
+        $filename = $file . '?v=' . $version;
+        if (!starts_with($filename, '/')) {
+            $filename = '/' . $filename;
+        }
+    }
+
+    $fileType = substr(strrchr($file, '.'), 1);
+
+    if ($fileType == 'js') {
+        return '<script src="' . $filename . '"></script>';
+    } else {
+        return '<link href="' . $filename . '" rel="stylesheet" />';
+    }
+});
+*/
+
     public function registerPackages(){
         $enable_packs = config('xra.enable_packs');
         $namespaces = [];
