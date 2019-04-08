e1f2cf26e2f6acb5411d2f1694321d88d311151f
marco xot <marco.sottana@gmail.com>
2019-04-08 20:24:04 +0200

    .

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -142,3 +142,35 @@ if (!\function_exists('in_admin')) {
         return 'admin' == \Request::segment(1);
     }
 }
+
+
+    /**
+     * Replaces spaces with full text search wildcards
+     *
+     * @param string $term
+     * @return string
+     */
+if (!\function_exists('fullTextWildcards')) {
+    /*protected */ function fullTextWildcards($term)
+    {
+        // removing symbols used by MySQL
+        $reservedSymbols = ['-', '+', '<', '>', '@', '(', ')', '~'];
+        $term = str_replace($reservedSymbols, '', $term);
+ 
+        $words = explode(' ', $term);
+ 
+        foreach($words as $key => $word) {
+            /*
+             * applying + operator (required word) only big words
+             * because smaller ones are not indexed by mysql
+             */
+            if(strlen($word) >= 3) {
+                $words[$key] = '+' . $word . '*';
+            }
+        }
+ 
+        $searchTerm = implode( ' ', $words);
+ 
+        return $searchTerm;
+    }
+}
